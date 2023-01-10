0908eb16125930ea19fe6cd8aa343ec9c28f0545
Dass7550 <dass7550@gmail.com>
2023-01-10 23:39:30 +0600

    profile picture added

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Card.css'
 const Card = (props) => {
-    const { _id, profile, price, name, email, phone, picture, rating } = props.site;
+    const { _id, profile, profile_picture, price, name, email, phone, picture, rating } = props.site;
     return (
         <div className='Card-Style'>
             <img className='Card-img' src={picture} alt="" />
             <div className='Card-Details'>
                 <h1>Spot Name: {name}</h1>
-                <h3>Profile Name: {profile}</h3>
+                <h3 className='prfile-picture-name'>
+                    <div className='profile-picture'><img src={profile_picture} alt="" srcset="" height={100} width={100} /></div>
+                    <div>{profile}</div>
+                </h3>
                 <p>Email: {email}</p>
                 <p>Phone: {phone}</p>
                 <p>Cost: {price}</p>
